c1fcebcce1feade39ccfb46e58001421f95e4766
Alex <izofatov2007@gmail.com>
2017-06-24 16:25:10 +0300

    start-addProd

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ function showFooter() {
     el.style.display = "block";
 
 }
+
+function createProduct() {
+
+    var name = document.getElementById("product_name").value;
+    var protein = document.getElementById("product_protein").value;
+    var fat = document.getElementById("product_fat").value;
+    var carbohydrates = document.getElementById("product_carbohydrates").value;
+    var price = document.getElementById("product_price").value;
+
+    if(name === "") {
+        var pl = document.getElementById("product_name");
+        pl.placeholder = "Вкажіть назву продукту";
+    }
+
+    // if(typeof protein === 'number') {
+    //     alert(asdfa);
+    // }
+
+    var product = {
+        name: name,
+        prot: protein,
+        fat: fat,
+        carb: carbohydrates,
+        price: price
+    }
+    
+    alert("Створено продукт: " + product.name + "\n" + product.prot + "\n" + product.fat + "\n"  + product.carb + "\n"  + product.price);
+    
+    addProduct(product);
+}
+
+function addProduct(prod) {
+    document.getElementsByClassName("main")[0].innerHTML = '<button>' + prod.name + '</button>' + prod.name;
+}
