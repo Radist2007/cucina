98092018f6580d06004677ca8a34113826207b36
Alex <izofatov2007@gmail.com>
2017-07-26 00:23:01 +0300

    hideLogin++

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 
 // show and hide sections of a form
 function preparePage() {
-
+        hideLogin = 0;
         document.getElementById("iconLogin").onclick = function() {
-            document.getElementById("indexLogin").style.display = "flex";
-            hideLogin = 1;
-            
+            if(hideLogin == 0) {
+                document.getElementById("indexLogin").style.display = "flex";
+                hideLogin = 1;
+            } else {
+                document.getElementById("indexLogin").style.display = "none";
+                hideLogin = 0;
+            }
         }
     }
 
